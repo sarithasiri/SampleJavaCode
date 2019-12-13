afcc5a9baf7ac85466f3d9c86e4a817ea3998334
saritha <saritha.bnd@gmail.com>
2019-12-13 23:15:33 +0530

    This is a master branch

---
+++ Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is a Sample Source code in Java");
+		for (int i=1; i<10 ;i++)
+			{
+			System.out.println("Count down ....... ["+i+"']");
+			}
+
+	}
+
+}
